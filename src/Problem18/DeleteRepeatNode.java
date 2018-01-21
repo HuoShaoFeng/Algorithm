20903a67fd50b481abc2c3bcd3e3ec712d56666e
HuoShaoFeng <huo_shao_feng@163.com>
2018-01-21 21:33:30 +0800

    新增算法： 1.将链表中重复的节点都删除掉； 2.将链表中重复的节点只保存一个； 3.正则表达式匹配问题；

--- src/Problem18/DeleteRepeatNode.java
+++ src/Problem18/DeleteRepeatNode.java
@@ -1,16 +1,19 @@
 package Problem18;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DeleteRepeatNode {
 
 	public static void main(String[] args) {
 		Node nh = new Node(1);
 		Node n1 = new Node(2);
 		Node n2 = new Node(2);
-		Node n3 = new Node(3);
-		Node n4 = new Node(3);
-		Node n5 = new Node(3);
-		Node n6 = new Node(5);
-		Node n7 = new Node(7);
+		Node n3 = new Node(5);
+		Node n4 = new Node(5);
+		Node n5 = new Node(5);
+		Node n6 = new Node(7);
+		Node n7 = new Node(9);
 		nh.next = n1;
 		n1.next = n2;
 		n2.next = n3;
@@ -21,24 +24,91 @@ public class DeleteRepeatNode {
 		n7.next = null;
 		System.out.println("删除前：");
 		PrintList(nh);
-		System.out.println("删除"+nh.data+"后：");
-		DeleteNodeList(nh);
-		PrintList(nh);
+		System.out.println("删除后：");
+		Node  nnn = DeleteNodeList(nh);
+//		DeleteNodeLeft1(nh);
+		PrintList(nnn);
+	}
+	
+	//删除重复的节点（留一个）
+	public static void DeleteNodeLeft1(Node head) {
+		Map<Integer,Integer> map = new HashMap<>();
+		Node cur = head;
+		Node nxt = cur.next;
+		if(cur==null)
+			return;
+		
+		if(!map.containsKey(cur.data)) {//没有存在
+			map.put(cur.data, 1);
+		}
+		
+		while(nxt!=null) {
+			
+			if(!map.containsKey(nxt.data)) {//没有存在
+				map.put(nxt.data, 1);
+				
+				cur.next = nxt;
+				cur = cur.next;
+				
+			}else {//已经存在了，说明重复了
+			}
+			nxt = nxt.next;
+		}
+		cur.next = null;
 	}
 
 	//删除链表中重复的节点
-	public static void DeleteNodeList(Node nh) {
+	public static Node DeleteNodeList(Node head) {
+		if(null == head)
+			return null;
 		
+		Node node = head;
+		Node pre = null;
 		
-		
+		while(node!=null) {
+			boolean isDel = false;
+			
+			Node nxt = null;
+			if(node.next!=null)
+				nxt = node.next;
+			
+			if(nxt!=null && node.data==nxt.data) {
+				isDel=true;
+			}
+			
+			if(isDel==false) {
+				pre = node;
+				node = node.next;
+			}
+			else {//需要删除
+				int value = node.data;
+				Node toDel = node;
+				while(toDel!=null && toDel.data==value) {
+					nxt = toDel.next;
+					toDel = null;
+					toDel = nxt;
+				}
+				if(pre==null) {
+					head = nxt;
+				}
+				else {
+					pre.next = nxt;
+				}
+				node = nxt;
+			}
+		}
+		return head;
 	}
 
 	public static void PrintList(Node nh) {
 		Node tmp = nh;
+		if(tmp==null)
+			System.out.println("null");
 		while(tmp!=null) {
-			System.out.println(tmp.data+"  ");
+			System.out.print(tmp.data+"  ");
 			tmp = tmp.next;
 		}
+		System.out.println();
 	}
 
 }
---
+++ src/Problem19/MatchRegx.java
@@ -0,0 +1,60 @@
+package Problem19;
+
+public class MatchRegx {
+
+	public static void main(String[] args) {
+
+		String str = "aaa";
+		String pattern = "aa*a";
+		
+//		System.out.println("0:"+str.substring(0));
+//		System.out.println("1:"+str.substring(1));
+//		System.out.println("2:"+str.substring(2).length()+",");
+		
+		boolean ret = match(str,pattern);
+		System.out.println(ret);
+	}
+	
+	public static boolean match(String str, String pattern) {
+		if(str==null || pattern == null)
+			return false;
+		
+		return matchCore(str, pattern);
+	}
+	
+	
+	public static boolean matchCore(String str, String pattern) {
+		
+		System.out.println(str+", "+pattern);
+		System.out.println(str.length()+", "+pattern.length());
+		
+		if(str.length()==0 && pattern.length()==0)
+			return true;
+		
+		if(str.length()>0 && pattern.length()==0)
+			return false;
+		
+		int s = 0,p=0;
+		
+		//模式串的下一个是 “*”
+		if((p+1)<pattern.length() && pattern.charAt(p+1)=='*') {
+			if(str.charAt(s)==pattern.charAt(p) || (pattern.charAt(p)=='.'&& str.length()>0)) {
+				boolean b = matchCore(str.substring(s+1),pattern.substring(p+2)) ||
+						matchCore(str.substring(s+1),pattern.substring(p)) ||
+						matchCore(str,pattern.substring(p+2));
+				return b;
+			}else {
+				return matchCore(str,pattern.substring(p+2));
+			}
+		}
+		
+		if((str.length()>0&&str.charAt(s)==pattern.charAt(p) )|| (pattern.charAt(p)=='.'&& str.length()>0)) {
+			return matchCore(str.substring(s+1),pattern.substring(p+1));
+		}
+		
+		
+		return false;
+	}
+	
+
+}

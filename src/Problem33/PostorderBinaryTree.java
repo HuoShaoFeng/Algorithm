9fc3eb1abc115b38e0fb54490727ee8140956329
HuoShaoFeng <huo_shao_feng@163.com>
2018-02-04 20:22:39 +0800

    commit test

---
+++ src/Problem33/PostorderBinaryTree.java
@@ -0,0 +1,17 @@
+package Problem33;
+
+/**
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果
+ * @author huosf
+ */
+public class PostorderBinaryTree {
+
+	public static void main(String[] args) {
+		
+	}
+	
+//	public static boolean verifySequenceOfBST(int[] seq,int tmp) {
+//		if(seq==null )
+//	}
+//	
+}
---
+++ src/Problem33/TreeNode.java
@@ -0,0 +1,10 @@
+package Problem33;
+
+public class TreeNode {
+	int data;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int value){
+		data = value;
+	}
+}

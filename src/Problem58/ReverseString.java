365c69ec106688ff8c9126d89a97c34ae37a9040
HuoShaoFeng <huo_shao_feng@163.com>
2018-02-09 12:01:01 +0800

    左旋转字符串

--- src/Problem58/ReverseString.java
+++ src/Problem58/ReverseString.java
@@ -6,8 +6,33 @@ public class ReverseString {
 		String str = "woshi I am a student. really";
 		
 		//输出
-		System.out.println(reverseWordOrder(str));
+//		System.out.println("翻转单词之间的顺序： "+reverseWordOrder(str));
 
+		String s = "abcdefg";
+		int n=2;
+		//左旋转字符串，变为cdefgab
+		System.out.println("左旋转字符串： "+leftRotateString(s,n));
+		
+	}
+
+	//左旋转字符串，abcdefg变为cdefgab
+	private static String leftRotateString(String s, int n) {
+		
+		if(s==null || s.length()<2 || n<1 || s.length()<=n)
+			return s;
+		
+		//整个字符串翻转
+		char[] charArray = s.toCharArray();
+		reverse(charArray, 0, charArray.length-1);
+		
+		//翻转左端
+		reverse(charArray, 0, charArray.length-n-1);
+		
+		//翻转右端
+		reverse(charArray, charArray.length-n, charArray.length-1);
+		
+		System.out.println("test: "+new String(charArray));
+		return new String(charArray);
 	}
 
 	private static String reverseWordOrder(String str) {

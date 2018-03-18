a4c1fab5e2db78c4b9c1fbdcdc2c9f4e479a8943
HuoShaoFeng <huo_shao_feng@163.com>
2018-03-18 15:56:24 +0800

    二叉树的镜像 非递归实现

--- src/Problem27/MirrorOfTree.java
+++ src/Problem27/MirrorOfTree.java
@@ -1,5 +1,10 @@
 package Problem27;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import Problem_32.Node;
 
 public class MirrorOfTree {
 
@@ -19,9 +24,54 @@ public class MirrorOfTree {
 		n5.left=null;n5.right=null;
 		n6.left=null;n6.right=null;
 
-		MirrorTree(troot1);
+		/*
+		 *           8
+		 *       8       7
+		 *     9   3  
+		 *        4 7 
+		 * 
+		 */
 		
+		
+		
+		
+		printTreeByLevel(troot1);
+		System.out.println();
+		
+//		MirrorTree(troot1);
+		MirrorTreeUnrecur(troot1);
+		printTreeByLevel(troot1);
 	}
+	
+	//非递归方式
+	public static void MirrorTreeUnrecur(TreeNode root) {
+		if(root==null)
+			return;
+		else {
+			Stack<TreeNode> stack = new Stack<>();
+			stack.push(root);
+			while(!stack.isEmpty()) {
+				root = stack.pop();
+				
+		        TreeNode temp;
+		        temp=root.right;
+		        root.right=root.left;
+		        root.left=temp;
+				
+		        if(root.left!=null)
+		        	stack.push(root.left);
+				if(root.right!=null)
+					stack.push(root.right);
+			}	
+		}		
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	//将二叉树 更改为 其 镜像树
 	public static void MirrorTree(TreeNode root) {
@@ -42,4 +92,35 @@ public class MirrorOfTree {
 		
 	}
 
+	
+	private static void printTreeByLevel(TreeNode root) {
+		if(root==null)
+			return;
+		Queue<TreeNode> queue = new LinkedList<>();
+		int level = 1;
+		TreeNode last = root;
+		TreeNode nLast = null;
+		queue.offer(root);
+		
+		System.out.print("Level "+(level++)+" : ");
+		
+		while(!queue.isEmpty()) {
+			root = queue.poll();
+			System.out.print(root.data+"  ");
+			if(root.left!=null) {
+				queue.offer(root.left);
+				nLast = root.left;
+			}
+			if(root.right!=null) {
+				queue.offer(root.right);
+				nLast = root.right;
+			}
+			
+			if(root==last && !queue.isEmpty()) {
+				System.out.println();
+				System.out.print("Level "+(level++)+" : ");
+				last = nLast;
+			}
+		}
+	}
 }

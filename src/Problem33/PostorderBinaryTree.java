a9394d4ce2be584a75faf13c593b0c94ada176bb
HuoShaoFeng <huo_shao_feng@163.com>
2018-03-20 10:21:22 +0800

    判断一个数组是不是某个二叉搜索树的后序遍历结果

--- src/Problem33/PostorderBinaryTree.java
+++ src/Problem33/PostorderBinaryTree.java
@@ -7,11 +7,42 @@ package Problem33;
 public class PostorderBinaryTree {
 
 	public static void main(String[] args) {
+		int[] seq = {7,4,6,5};//{5,7,6,9,11,10,8}
 		
+		System.out.println(verifySequenceOfBST(seq, 0, seq.length-1));
 	}
 	
-//	public static boolean verifySequenceOfBST(int[] seq,int tmp) {
-//		if(seq==null )
-//	}
-//	
+	public static boolean verifySequenceOfBST(int[] seq,int start, int end) {
+		if(seq==null || seq.length<=0 )
+			return false;
+		
+		int root = seq[end];//后序遍历中最后一个就是根节点
+		
+		//在二叉搜索树中 左子树的节点的值 小于该父节点的值
+		int i = 0;
+		for(;i<end;++i) {
+			if(seq[i]>root)
+				break;
+		}
+		
+		//在二叉搜索树中 右子树的节点的值 大于该父节点的值
+		int j = i;
+		for(;j<end;++j) {
+			if(seq[j]<root)
+				return false; //如果有一个节点小于根节点，直接返回false
+		}
+		
+		//判断左子树是不是二叉搜索树
+		boolean left = true;
+		if(i>0)
+			left = verifySequenceOfBST(seq,0,i-1);
+		
+		//判断右子树是否二叉搜索树
+		boolean right = true;
+		if(i<end) {
+			right = verifySequenceOfBST(seq,i,end-1);
+		}
+		return left&&right;
+	}
+	
 }
